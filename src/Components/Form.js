05051940eb3d504500b5e90d0685a3f3236b26b4
lawpal86 <67480487+lawpal86@users.noreply.github.com>
2020-08-13 19:57:27 -0500

    created class form

---
+++ src/Components/Form.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+
+class Form extends Component {
+    constructor() {
+        super()
+        this.state = {
+            
+        }
+    }
+}
+
+export default Form; 
