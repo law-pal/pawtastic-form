722af9633e85e1ec6652a6bd72400ac40d6fcaf8
lawpal86 <67480487+lawpal86@users.noreply.github.com>
2020-08-13 20:28:07 -0500

    added handle events

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -10,7 +10,32 @@ class Form extends Component {
             gender: '',
             weight: ''
         }
+        this.handleNameChange = this.handleNameChange.bind(this);
+        this.handleBreedChange = this.handleBreedChange.bind(this);
+        this.handleBirthdayChange = this.handleBirthdayChange.bind(this);
+        this.handleGenderChange = this.handleGenderChange.bind(this);
+        this.handleWeightChange = this.handleWeightChange.bind(this);
     }
+
+    handleNameChange(event) {
+        this.setState({name: event.target.value});
+    } 
+
+    handleBreedChange(event) {
+        this.setState({breed: event.target.value});
+    } 
+
+    handleBirthdayChange(event) {
+        this.setState({birthday: event.target.value});
+    } 
+
+    handleGenderChange(event) {
+        this.setState({gender: event.target.value});
+    } 
+
+    handleWeightChange(event) {
+        this.setState({weight: event.target.value});
+    } 
 }
 
 export default Form; 
